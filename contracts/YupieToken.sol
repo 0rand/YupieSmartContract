70382346d0f7c28de6521c9cee8bade23c5d6f92
voilageapp <tonychrisfronda@gmail.com>
2017-08-14 13:44:12 -0500

    testing on kovan

--- contracts/YupieToken.sol
+++ contracts/YupieToken.sol
@@ -16,45 +16,42 @@ contract YupieToken is StandardToken {
 	string public version = "1.0";
 
 	// YUPIE TOKEN PURCHASE LIMITS
-	uint256 public constant maxTotalSupply = 631*1000000*1000000000000000000; 	// MAX TOTAL YUPIES 631 million
-	uint256 public constant maxPresaleSupply = maxTotalSupply*8/1000; 			// MAX TOTAL DURING PRESALE (0.8% of MAXTOTALSUPPLY)
+	uint256 public maxPresaleSupply; 														// MAX TOTAL DURING PRESALE (0.8% of MAXTOTALSUPPLY)
 
 	// PURCHASE DATES
-	uint256 public constant preSaleStartTime = 0; //1502784000; 				// GMT: Tuesday, August 15, 2017 8:00:00 AM
-	uint256 public constant preSaleEndTime = 1505671200; 						// GMT: Sunday, September 17, 2017 6:00:00 PM
-	uint256 public constant saleStartTime = 1509523200; 						// GMT: Wednesday, November 1, 2017 8:00:00 AM
-	uint256 public constant saleEndTime = 1512115200; 							// GMT: Friday, December 1, 2017 8:00:00 AM
+	uint256 public constant preSaleStartTime = 0; //1502784000; 							// GMT: Tuesday, August 15, 2017 8:00:00 AM
+	uint256 public constant preSaleEndTime = 1505671200; 									// GMT: Sunday, September 17, 2017 6:00:00 PM
+	uint256 public constant saleStartTime = 1509523200; 									// GMT: Wednesday, November 1, 2017 8:00:00 AM
+	uint256 public constant saleEndTime = 1512115200; 										// GMT: Friday, December 1, 2017 8:00:00 AM
 
 	// PURCHASE BONUSES
-	uint256 public constant tenPercentEtherBonuses = 5 * 1 ether; 				// 5+ Ether
-	uint256 public constant fiftenPercentEtherBonuses = 24 * 1 ether; 			// 24+ Ether
-	uint256 public constant twentyPercentEtherBonuses = 50 * 1 ether; 			// 50+ Ether
-	uint256 public constant twentyPercentDayBonuses = 0; 						// 1-12 Days
-	uint256 public constant fifteenPercentDayBonuses = 1036800; 				// 12-24 Days
-	uint256 public constant tenPercentDayBonuses = 2073600;						// 24+ Days
+	uint256 public constant tenPercentEtherBonuses = 5 * 1 ether; 							// 5+ Ether
+	uint256 public constant fiftenPercentEtherBonuses = 24 * 1 ether; 						// 24+ Ether
+	uint256 public constant twentyPercentEtherBonuses = 50 * 1 ether; 						// 50+ Ether
+	uint256 public constant twentyPercentDayBonuses = 0; 									// 1-12 Days
+	uint256 public constant fifteenPercentDayBonuses = 1036800; 							// 12-24 Days
+	uint256 public constant tenPercentDayBonuses = 2073600;									// 24+ Days
 
 	// PRICING INFO
-	uint256 public constant YUPIE_PER_ETH_PRE_SALE = 3000;  					// 3000 YUPIE = 1 ETH
-	uint256 public constant YUPIE_PER_ETH_SALE = 1000;  						// 1000 YUPIE = 1 ETH
+	uint256 public constant YUPIE_PER_ETH_PRE_SALE = 3000;  								// 3000 YUPIE = 1 ETH
+	uint256 public constant YUPIE_PER_ETH_SALE = 1000;  									// 1000 YUPIE = 1 ETH
 	
 	// ADDRESSES
-	address public constant ownerAddress = 0x616263; 							// This contracts address
-	address public constant crowdholdingAddress = 0x616263;	  					// Crowdholding's wallet
+	address public constant ownerAddress = 0x5E31408E65937713883cA60a5396cb4524c9288b; 		// The owners address
 
 	// STATE INFO	
-	bool public allowInvestment = true;											// Flag to change if transfering is allowed
-	uint256 public totalWEIInvested = 0; 										// Total WEI invested
-	uint256 public totalYUPIESAllocated = 0;									// Total YUPIES allocated
-	mapping (address => uint256) public WEIContributed; 						// Total WEI Per Account
+	bool public allowInvestment = true;														// Flag to change if transfering is allowed
+	uint256 public totalWEIInvested = 0; 													// Total WEI invested
+	uint256 public totalYUPIESAllocated = 0;												// Total YUPIES allocated
+	mapping (address => uint256) public WEIContributed; 									// Total WEI Per Account
 
 
 	// INITIALIZATIONS FUNCTION
 	function YupieToken() {
-		
-		// CHECK VALID ADDRESSES
-		require(ownerAddress != address(0x0));
-		require(crowdholdingAddress != address(0x0));
+		require(msg.sender == ownerAddress);
 
+		totalSupply = 631*1000000*1000000000000000000; 										// MAX TOTAL YUPIES 631 million
+		maxPresaleSupply = totalSupply*8/1000; 											// MAX TOTAL DURING PRESALE (0.8% of MAXTOTALSUPPLY)
 	}
 
 
@@ -84,7 +81,7 @@ contract YupieToken is StandardToken {
 			amountOfYUPIE = amountOfWei.mul(YUPIE_PER_ETH_SALE);
 			tenPercentBonusTime = saleStartTime + tenPercentDayBonuses;
 			fifteenPercentBonusTime = saleStartTime + fifteenPercentDayBonuses;
-			totalYUPIEAvailable = maxTotalSupply - totalYUPIESAllocated;
+			totalYUPIEAvailable = totalSupply - totalYUPIESAllocated;
 		} else {
 			// Invalid investment period
 			revert();
@@ -94,16 +91,16 @@ contract YupieToken is StandardToken {
 		assert(amountOfYUPIE > 0);
 
 		// Apply Bonuses
-		if (amountOfWei > twentyPercentEtherBonuses) {
+		if (amountOfWei >= twentyPercentEtherBonuses) {
 			amountOfYUPIE = amountOfYUPIE.mul(12).div(10);
-		} else if (amountOfWei > fiftenPercentEtherBonuses) {
+		} else if (amountOfWei >= fiftenPercentEtherBonuses) {
 			amountOfYUPIE = amountOfYUPIE.mul(115).div(100);
-		} else if (amountOfWei > tenPercentEtherBonuses) {
+		} else if (amountOfWei >= tenPercentEtherBonuses) {
 			amountOfYUPIE = amountOfYUPIE.mul(11).div(10);
 		}
-		if (block.timestamp > (tenPercentBonusTime)) {
+		if (block.timestamp >= (tenPercentBonusTime)) {
 			amountOfYUPIE = amountOfYUPIE.mul(11).div(10);
-		} else if (block.timestamp > (fifteenPercentBonusTime)) {
+		} else if (block.timestamp >= (fifteenPercentBonusTime)) {
 			amountOfYUPIE = amountOfYUPIE.mul(115).div(100);
 		} else {
 			amountOfYUPIE = amountOfYUPIE.mul(12).div(10);
@@ -137,14 +134,6 @@ contract YupieToken is StandardToken {
 	}
 	
 	
-	// CHANGE PARAMETERS METHODS
-	function transferEther(address addressToSendTo, uint256 value) {
-		require(msg.sender == ownerAddress);
-		addressToSendTo.transfer(value);
-	}	
-	function changeAllowInvestment(bool _allowInvestment) {
-		require(msg.sender == ownerAddress);
-		allowInvestment = _allowInvestment;
-	}
+	
 
 }
--- test/tests.js
+++ test/tests.js
@@ -1,47 +1,81 @@
 var YupieToken = artifacts.require("YupieToken");
 
 contract('YupieToken', function(accounts) {
+  var yupie;
   it("should initialize with 631 million YUPIES", function() {
     return YupieToken.deployed().then(function(instance) {
-      // console.log('initialize')
-      // console.log(accounts);
-      // console.log(web3);
-      return instance.maxTotalSupply.call();
+      yupie = instance;
+      return yupie.maxTotalSupply.call();
     }).then(function(balance) {
-      // console.log('returned')
-      // console.log(balance.valueOf())
-      assert.equal(balance.valueOf(), 6.31e+26, "6.31e+26 wasn't in the first account");
+      assert.equal(balance.valueOf(), 6.31e+26);
+      return yupie.maxPresaleSupply.call();
+    }).then(function(balance) {
+      assert.equal(balance.valueOf(), 5.048e+24);
     });
   });
   it("should send coin correctly", function() {
     var yupie;
-
-    // Get initial balances of first and second account.
-    var account_one = accounts[0];
-    var account_two = accounts[1];
-
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
-
-    var amount = 10;
-
     return YupieToken.deployed().then(function(instance) {
       yupie = instance;
       return yupie.totalYUPIESAllocated();
     }).then(function(totalYupies) {
-      assert.equal(totalYupies.toString(), '0', 'Was not zero balance');
-      console.log(totalYupies.toString())
-      return yupie.sendTransaction({from:account_one,value:5});
+      assert.equal(totalYupies.toString(), '0');
+      return yupie.sendTransaction({from:accounts[0],value:5});
     }).then(function(balance) {
       return yupie.totalWEIInvested();
     }).then(function(totalWei) {
-      assert.equal(totalWei.toString(), '5', 'Total wei was not 5');
+      assert.equal(totalWei.toString(), '5');
       return yupie.totalYUPIESAllocated();
     }).then(function(totalYupies) {
-      console.log(totalYupies.toString())
-      assert.equal(totalYupies.toString(), '16500', 'Was not zero balance');
-    });
+      assert.equal(totalYupies.toString(), '16500');
+      return yupie.WEIContributed(accounts[0]);
+    }).then(function(weiContributed) {
+      assert.equal(weiContributed.toString(), '5');
+      return yupie.sendTransaction({from:accounts[1],value:5000});
+    }).then(function(balance) {
+      return yupie.totalWEIInvested();
+    }).then(function(totalWei) {
+      assert.equal(totalWei.toString(), '5005');
+      return yupie.totalYUPIESAllocated();
+    }).then(function(totalYupies) {
+      assert.equal(totalYupies.toString(), '16516500');
+      return yupie.WEIContributed(accounts[1]);
+    }).then(function(weiContributed) {
+      assert.equal(weiContributed.toString(), '5000');
+      return yupie.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toString(), '16500');
+      return yupie.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toString(), '16500000');
+      return yupie.sendTransaction({from:accounts[0],value:5000000000000000000});
+    }).then(function(balance) {
+      return yupie.totalWEIInvested();
+    }).then(function(totalWei) {
+      assert.equal(totalWei.toString(), '5000000000000005005');
+      return yupie.totalYUPIESAllocated();
+    }).then(function(totalYupies) {
+      assert.equal(totalYupies.toString(), '1.81500000000000165165e+22');
+      return yupie.WEIContributed(accounts[0]);
+    }).then(function(weiContributed) {
+      assert.equal(weiContributed.toString(), '5000000000000000005');
+      return yupie.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toString(), '1.81500000000000000165e+22');
+      return yupie.sendTransaction({from:accounts[1],value:5000000000000000000});
+    }).then(function(balance) {
+      return yupie.totalWEIInvested();
+    }).then(function(totalWei) {
+      assert.equal(totalWei.toString(), '55000000000000005005');
+      return yupie.totalYUPIESAllocated();
+    }).then(function(totalYupies) {
+      assert.equal(totalYupies.toString(), '1.81500000000000165165e+22');
+      return yupie.WEIContributed(accounts[1]);
+    }).then(function(weiContributed) {
+      assert.equal(weiContributed.toString(), '5000000000000000005');
+      return yupie.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toString(), '1.81500000000000000165e+22');
+    })
   });
 });
